fb8bca71cb04a69a5d0deb0582825987181d5a5e
Shreyas Sachin Gaidhani <shreyassgaidhani@gmail.com>
2026-08-04 17:29:50 +0000

    Sync LeetCode submission Runtime - 1 ms (98.95%), Memory - 44.8 MB (67.95%)

---
+++ 0414-third-maximum-number/solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        long max = Long.MIN_VALUE;
+        long secondmax = Long.MIN_VALUE;
+        long thirdmax = Long.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==max || nums[i]==secondmax || nums[i]==thirdmax)
+            continue;
+            if (nums[i] > max)
+            {
+                thirdmax=secondmax;
+                secondmax=max;
+                max=nums[i];
+            }
+            else if(nums[i]>secondmax)
+            {
+                thirdmax=secondmax;
+                secondmax=nums[i];
+            }
+            else if(nums[i]>thirdmax)
+            thirdmax=nums[i];
+            
+
+        }
+            if(thirdmax==Long.MIN_VALUE)
+            return (int)max;
+            else
+            return (int)thirdmax;
+    }
+    }
+
+
