f1e2df7efb303900ba59a1013ec9b4e708b1714b
Shreyas Sachin Gaidhani <shreyassgaidhani@gmail.com>
2026-08-09 19:48:12 +0000

    Sync LeetCode submission Runtime - 2 ms (97.11%), Memory - 44.9 MB (69.87%)

--- 0349-intersection-of-two-arrays/solution.java
+++ 0349-intersection-of-two-arrays/solution.java
@@ -1,33 +1,28 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set1= new HashSet<>();
-        int left=0;
-        while(left<nums1.length)
+        HashSet<Integer> set1 = new HashSet<>();
+        for(int i=0;i<nums1.length;i++)
         {
-            set1.add(nums1[left]);
-            left++;
+            set1.add(nums1[i]);
         }
-        int right=0;
-        HashSet<Integer> set2=new HashSet<>();
-        while(right<nums2.length)
-        {
-            if(set1.contains(nums2[right]))
-                set2.add(nums2[right]);
-                right++;
-        }
-        int n= set2.size();
-        int[] arr = new int[n];
+    HashSet<Integer> set2 = new HashSet<>();
+    int i=0;
+    for(int j =0;j<nums2.length;j++)
+    {
+        if(set1.contains(nums2[j]))
+        set2.add(nums2[j]);
+        i++;
+    } 
+            int[] commonarr = new int[set2.size()];
+
         Iterator<Integer> it = set2.iterator();
-        int i=0;
-        while(it.hasNext())
-        {
-            arr[i]=it.next();
-            i++;
-        }
-           return arr;
+
+int k=0;
+    while(it.hasNext())
+    {
+        commonarr[k] = it.next();
+        k++;
+     }
+     return commonarr;
     }
-}
-
-          
-            
-
+}  
